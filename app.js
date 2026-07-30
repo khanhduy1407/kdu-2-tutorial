812839b6492d153d05b0a6ab9e63154c94968c89
NKDuy <nkduy.dev@gmail.com>
2026-07-30 15:08:08 +0700

    Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Kdu({
   el: "#kdu-app",
   data: {
     name: "NKDuy",
-    job: "Full-stack Software Engineer"
+    job: "Full-stack Software Engineer",
+    website: "https://nkduy-cloud.web.app",
+    websiteTag: "<a href='https://nkduy-cloud.web.app'>NKDuy Cloud</a>"
   },
   methods: {
     greet: function (time) {
