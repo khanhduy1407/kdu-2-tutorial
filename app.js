c8c477d6275c76a1c19df9865d3bf073a222c014
NKDuy <nkduy.dev@gmail.com>
2026-07-30 18:48:08 +0700

    Two-Way Data Binding

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 new Kdu({
   el: "#kdu-app",
   data: {
+    name: "",
+    age: ""
   },
   methods: {
-    logName: function () {
-      console.log("you entered your name");
-    },
-    logAge: function () {
-      console.log("you entered your age");
-    },
-    logJob: function () {
-      console.log("you entered your job");
-    }
   }
 })
