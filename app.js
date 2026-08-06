cd3d0279b3949761bb74b07ce5d0d2ee79ada785
NKDuy <nkduy.dev@gmail.com>
2026-08-06 14:01:34 +0700

    Looping with k-for

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 new Kdu({
   el: "#kdu-app",
   data: {
-    error: false,
-    success: false
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bower'],
+    ninjas: [
+      { name: 'Ryu', age: 25 },
+      { name: 'Yoshi', age: 35 },
+      { name: 'Ken', age: 55 },
+    ]
   },
   methods: {
   },
