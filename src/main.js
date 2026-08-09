7d8896b3b1b3eed4539edc406b824be17a6da5b9
NKDuy <nkduy.dev@gmail.com>
2026-08-09 15:40:18 +0700

    Custom Directives

--- src/main.js
+++ src/main.js
@@ -4,6 +4,28 @@ import KduResource from 'kdu-resource'
 
 Kdu.use(KduResource);
 
+// Custom directives
+Kdu.directive('rainbow', {
+  bind(el, binding, knode) {
+    el.style.color = "#" + Math.random().toString().slice(2, 8);
+  }
+})
+
+Kdu.directive('theme', {
+  bind(el, binding, knode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = "560px";
+    }
+
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+})
+
 new Kdu({
   el: '#app',
   render: h => h(App)
