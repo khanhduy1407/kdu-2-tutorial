06ccc3a03cac47d5c25a77842ef451289ba7405a
NKDuy <nkduy.dev@gmail.com>
2026-08-10 09:02:51 +0700

    Route Parameters - Importing Route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import showBlogs from './components/showBlogs.kdu';
 import addBlog from './components/addBlog.kdu';
+import singleBlog from './components/singleBlog.kdu';
 
 export default [
   { path: '/', component: showBlogs },
-  { path: '/add', component: addBlog }
+  { path: '/add', component: addBlog },
+  { path: '/blog/:id', component: singleBlog }
 ]
